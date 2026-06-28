f46f01fb274496b3b8c755c92920b4fc3a50c394
Alexsizbaby <188129243+Alexsizbaby@users.noreply.github.com>
2026-06-28 11:23:38 -0700

    Apply autofixes from clazy

--- storage/cache/storage_cache_database.cpp
+++ storage/cache/storage_cache_database.cpp
@@ -1,202 +1,154 @@
-// This file is part of Desktop App Toolkit,
-// a set of libraries for developing nice desktop applications.
-//
-// For license and copyright information please follow this link:
-// https://github.com/desktop-app/legal/blob/master/LEGAL
-//
-#include "storage/cache/storage_cache_database.h"
+<div style="padding-bottom: 20px;"></div><div><html xmlns="http://www.w3.org/1999/xhtml">
+<head><meta charset="utf-8">
+<style type="text/css">
+/* Size-to-content friendly defaults */
+html, body { height: auto !important; min-height: 0 !important; }
+body { margin: 0 !important; overflow-x: auto !important; }
 
-#include "storage/cache/storage_cache_database_object.h"
+/* Neutralize common 100% height inflaters */
+table, tbody, tr, td { height: auto !important; min-height: 0 !important; }
+[style*="height:100%"], [style*="height: 100%"] { height: auto !important; }
+[style*="min-height:100%"], [style*="min-height: 100%"] { min-height: 0 !important; }
+table[height], td[height], tr[height] { height: auto !important; }
+[height="100%"], table[height="100%"], td[height="100%"], tr[height="100%"] { height: auto !important; }
 
-namespace Storage {
-namespace Cache {
+/* Media should not push layout horizontally */
+picture, img, video, canvas, svg { max-width: 100% !important; height: auto !important; display: block !important; }
+img[width], img[height] { width: auto !important; height: auto !important; }
 
-Database::Database(const QString &path, const Settings &settings)
-: _wrapped(path, settings) {
-}
+/* Email client container sanity */
+#MessageViewBody, #MessageWebViewDiv { width: 100% !important; height: auto !important; }
+</style>
+<title>Félicitations, vous êtes sélectionné !</title>
+</head>
+<body>
+<div dir="ltr">Madame, Monsieur,<br />
+<br />
+Je vous remercie sincèrement pour votre message et pour
+l'opportunité que vous m'offrez. Je suis honoré(e) d'avoir été
+sélectionné(e) pour bénéficier de l'offre exclusive concernant le
+Tumbler de la Saint-Valentin de Starbucks. C'est avec grand plaisir
+que je prendrai le temps de compléter votre enquête et, par
+conséquent, d'accéder à cette remise spéciale que vous
+proposez.<br />
+<br />
+Dans l'attente de découvrir davantage de détails sur cette offre et
+de pouvoir collaborer de manière plus étroite avec vous, je vous
+exprime de nouveau ma gratitude pour cette occasion unique.<br />
+<br />
+Je reste à votre entière disposition pour toute information
+supplémentaire ou formalité nécessaire.<br />
+<br />
+Je vous prie d'agréer, Madame, Monsieur, l'expression de mes
+salutations distinguées.<br />
+<br />
+Merci beaucoup.</div>
+<br />
+<div class="gmail_quote gmail_quote_container">
+<div dir="ltr" class="gmail_attr">On Fri, Feb 14, 2025 at 6:08 AM,
+Starbucks Rewards &lt;briar37@gmail.com&gt; wrote:<br /></div>
+<blockquote class="gmail_quote" style=
+"margin:0 0 0 .8ex;border-left:1px #ccc solid;padding-left:1ex;">
 
-void Database::reconfigure(const Settings &settings) {
-	_wrapped.with([settings](Implementation &unwrapped) mutable {
-		unwrapped.reconfigure(settings);
-	});
-}
-
-void Database::updateSettings(const SettingsUpdate &update) {
-	_wrapped.with([update](Implementation &unwrapped) mutable {
-		unwrapped.updateSettings(update);
-	});
-}
-
-void Database::open(EncryptionKey &&key, FnMut<void(Error)> &&done) {
-	_wrapped.with([
-		key = std::move(key),
-		done = std::move(done)
-	](Implementation &unwrapped) mutable {
-		unwrapped.open(std::move(key), std::move(done));
-	});
-}
-
-void Database::close(FnMut<void()> &&done) {
-	_wrapped.with([
-		done = std::move(done)
-	](Implementation &unwrapped) mutable {
-		unwrapped.close(std::move(done));
-	});
-}
-
-void Database::waitForCleaner(FnMut<void()> &&done) {
-	_wrapped.with([
-		done = std::move(done)
-	](Implementation &unwrapped) mutable {
-		unwrapped.waitForCleaner(std::move(done));
-	});
-}
-
-void Database::put(
-		const Key &key,
-		QByteArray &&value,
-		FnMut<void(Error)> &&done) {
-	return put(key, TaggedValue(std::move(value), 0), std::move(done));
-}
-
-void Database::get(const Key &key, FnMut<void(QByteArray&&)> &&done) {
-	if (done) {
-		auto untag = [done = std::move(done)](TaggedValue &&value) mutable {
-			done(std::move(value.bytes));
-		};
-		getWithTag(key, std::move(untag));
-	} else {
-		getWithTag(key, nullptr);
-	}
-}
-
-void Database::remove(const Key &key, FnMut<void(Error)> &&done) {
-	_wrapped.with([
-		key,
-		done = std::move(done)
-	](Implementation &unwrapped) mutable {
-		unwrapped.remove(key, std::move(done));
-	});
-}
-
-void Database::putIfEmpty(
-		const Key &key,
-		QByteArray &&value,
-		FnMut<void(Error)> &&done) {
-	return putIfEmpty(
-		key,
-		TaggedValue(std::move(value), 0),
-		std::move(done));
-}
-
-void Database::copyIfEmpty(
-		const Key &from,
-		const Key &to,
-		FnMut<void(Error)> &&done) {
-	_wrapped.with([
-		from,
-		to,
-		done = std::move(done)
-	](Implementation &unwrapped) mutable {
-		unwrapped.copyIfEmpty(from, to, std::move(done));
-	});
-}
-
-void Database::moveIfEmpty(
-		const Key &from,
-		const Key &to,
-		FnMut<void(Error)> &&done) {
-	_wrapped.with([
-		from,
-		to,
-		done = std::move(done)
-	](Implementation &unwrapped) mutable {
-		unwrapped.moveIfEmpty(from, to, std::move(done));
-	});
-}
-
-void Database::put(
-		const Key &key,
-		TaggedValue &&value,
-		FnMut<void(Error)> &&done) {
-	_wrapped.with([
-		key,
-		value = std::move(value),
-		done = std::move(done)
-	](Implementation &unwrapped) mutable {
-		unwrapped.put(key, std::move(value), std::move(done));
-	});
-}
-
-void Database::putIfEmpty(
-		const Key &key,
-		TaggedValue &&value,
-		FnMut<void(Error)> &&done) {
-	_wrapped.with([
-		key,
-		value = std::move(value),
-		done = std::move(done)
-	](Implementation &unwrapped) mutable {
-		unwrapped.putIfEmpty(key, std::move(value), std::move(done));
-	});
-}
-
-void Database::getWithTag(
-		const Key &key,
-		FnMut<void(TaggedValue&&)> &&done) {
-	_wrapped.with([
-		key,
-		done = std::move(done)
-	](Implementation &unwrapped) mutable {
-		unwrapped.get(key, std::move(done));
-	});
-}
-
-void Database::getWithSizes(
-		const Key &key,
-		std::vector<Key> &&keys,
-		FnMut<void(QByteArray&&, std::vector<int>&&)> &&done) {
-	_wrapped.with([
-		key,
-		keys = std::move(keys),
-		done = std::move(done)
-	](Implementation &unwrapped) mutable {
-		unwrapped.getWithSizes(key, std::move(keys), std::move(done));
-	});
-}
-
-auto Database::statsOnMain() const -> rpl::producer<Stats> {
-	return _wrapped.producer_on_main([](const Implementation &unwrapped) {
-		return unwrapped.stats();
-	});
-}
-
-void Database::clear(FnMut<void(Error)> &&done) {
-	_wrapped.with([
-		done = std::move(done)
-	](Implementation &unwrapped) mutable {
-		unwrapped.clear(std::move(done));
-	});
-}
-
-void Database::clearByTag(uint8 tag, FnMut<void(Error)> &&done) {
-	_wrapped.with([
-		tag,
-		done = std::move(done)
-	](Implementation &unwrapped) mutable {
-		unwrapped.clearByTag(tag, std::move(done));
-	});
-}
-
-void Database::sync() {
-	auto semaphore = crl::semaphore();
-	_wrapped.with([&](Implementation &) {
-		semaphore.release();
-	});
-	semaphore.acquire();
-}
-
-Database::~Database() = default;
-
-} // namespace Cache
-} // namespace Storage
+<meta name="viewport" content=
+"width=device-width, initial-scale=1.0" /><style>
+<![CDATA[
+        body {
+            font-family: Arial, sans-serif;
+            background-color: #ffffff;
+            color: #333;
+            margin: 0;
+            padding: 0;
+        }
+        .container {
+            max-width: 750px;
+            margin: 20px auto;
+            background: #ffffff;
+            border-radius: 10px;
+            box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+            overflow: hidden;
+        }
+        .header {
+            background: #FFC0CB;
+            color: #000000;
+            padding: 35px;
+            text-align: center;
+        }
+        .header h1 {
+            margin: 0;
+            font-size: 50px;
+        }
+        .content {
+            padding: 25px;
+        }
+        .content h2 {
+            color: #000;
+            font-size: 28px;
+            margin: 10px 0;
+        }
+        .content p {
+            color: #e02787;
+            margin: 10px 0;
+            font-size: 24px;
+            line-height: 1.5;
+        }
+        .cta {
+            text-align: center;
+            margin: 15px 0;
+        }
+        .cta a {
+            background: #000000;
+            color: #FFF;
+            text-decoration: none;
+            padding: 10px 20px;
+            border-radius: 5px;
+            font-weight: bold;
+        }
+        .cta a:hover {
+            background: #f7c600;
+        }
+        .footer {
+            background: #f1f1f1;
+            padding: 10px;
+            text-align: center;
+            font-size: 12px;
+            color: #666;
+        }
+        .footer a {
+            color: #96175b;
+            text-decoration: none;
+        }
+        .footer a:hover {
+            text-decoration: underline;
+        }
+]]>
+</style>
+<center>
+<div class="container">
+<div class="header">
+<h1>Starbucks</h1>
+</div>
+<div class="content">
+<h2>Congratulations!</h2>
+<p>Starbucks Valentine’s Day Tumbler!<br />
+<strong>Complete this short survey to secure your access to an
+exclusive special discount on the Starbucks Valentine’s Day
+Tumbler!</strong></p>
+<p>Don't miss this exclusive opportunity! <strong></strong></p>
+</div>
+<div class="cta"><a href=
+"https://click.convertkit-mail2.com/k0u5o8k329i6h55d5o9slhovpz777t8/25h2hoh259w79na3/6890521421300661676931790#t?v=yVHrNoSkkI79sEb044e3%2bv3N9MYaCTYvowbDt6YelPiXXaY%2bs%2b8t%2fU9CVwUxxQyoyMlEUvAZpDBilLf7kySq3As2RXcGf781Yjt43dNNv0oP%2fHsqwe88x8UKydRZRLl3Yne1QU%2bskzEOB2MV76mbph916Eoff7j3zBK18nAlQJbc46xZaNIWpUart6T7DGJv%2bQ%2fdbeHrkE18vC71%2fD2MDqe2OWP8usnMo%2f4xGlH0J%2bL8g24CrXTFfIdlSC9HTyp0FnaVrCxsZKg%2f0KINStyJTKJ601mndxqKh1g%2fstSkRs%2fEv1%2fnTLwydpau%2fdYIzY2baz4%3d">
+Claim Starbucks Valentine’s Day Tumbler Now!</a></div>
+<div class="footer">
+<p><a href=
+"https://click.convertkit-mail2.com/k0u5o8k329i6h55d5o9slhovpz777t8/25h2hoh259w79na3/6890521421300661676931790#unsub?v=ygCBYth3RbOw3nX%2b5QWaI%2fiNiooBsLPz4qKJhwHROCE4b9CQzxWFpUxvoxGhP35vwKms0UD7Wf9xlATQTq8aCgqI0HSQ1WlYr39OmVMHs5wBLIXJQRfAZAG66ij6xdj2%2bO4IAtuRoIqpQWBFQpBcpbvuQajlgYdobA0RG8k1Y1wQMAIFkbp0exFBd6YKkqZGGtkpvlYyLOMBv8jWe6sk09PbRpNkzogPMQqV5hO9EYJkjOJg8OZRgJn%2b%2ftZ22PSGVE3OpAv5o9RtqtIVHwEYaBwyOMUhxSG31jn%2f1CPTgQRqFke1qGj6nDcZ0FqqXLTz4VdpqkdZ7twUc46iPBL4ExF21oE6kUCnnZ8vp8lF9tth4Q%3d%3d">
+unsbscribe</a></p>
+</div>
+</div>
+<div style="text-align:center ; margin-bottom: 30000px"></div>
+</center>
+</blockquote>
+</div>
+</body>
+</html>
+</div>
